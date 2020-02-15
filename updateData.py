5befdd56ca61d3c4fd8a1923dc6acd6b2618e7bc
Semidán Robaina-Estévez <tintanjarafe@gmail.com>
2020-02-15 15:44:00 +0000

    added python update data file

--- updateData.py
+++ updateData.py
@@ -1,17 +1,11 @@
 """
 Update google trend data and push to repository
 """
-# import requests
 from subprocess import call
 
 workDir = 'C:/Users/tinta/OneDrive/Documents/Web_development/googleTrends/'
 
-# Download new data file
-# url = 'http://risp.puertosdetenerife.org/dataset/eff95e11-4baa-4ab8-aeb2-33d80c6395d8/resource/4b31504e-fd63-4eba-a9ef-6663a12d5dd0/download/crucerosprevistos.csv'
-# myfile = requests.get(url)
-# open(workDir + 'crucerosprevistos_Tenerife.csv', 'wb').write(myfile.content)
-
 # Push commit
 call(['node', 'updateData.js'], cwd=workDir)
-# call(['git', 'commit', '-m', '"Update data"'], cwd=workDir)
-# call(['git', 'push', 'origin', 'master'], cwd=workDir)
+call(['git', 'commit', '-m', '"Update data"'], cwd=workDir)
+call(['git', 'push', 'origin', 'master'], cwd=workDir)
