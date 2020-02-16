ca8b2051040e3e7124b5048a6c883901fd610bd6
Semidán Robaina-Estévez <tintanjarafe@gmail.com>
2020-02-16 09:44:22 +0000

    typo python

--- updateData.py
+++ updateData.py
@@ -7,5 +7,6 @@ workDir = 'C:/Users/tinta/OneDrive/Documents/Web_development/googleTrends/'
 
 # Push commit
 call(['node', 'updateData.js'], cwd=workDir)
+call(['git', 'add', 'CountryTrends.json'], cwd=workDir)
 call(['git', 'commit', '-m', '"Update data"'], cwd=workDir)
 call(['git', 'push', 'origin', 'master'], cwd=workDir)
